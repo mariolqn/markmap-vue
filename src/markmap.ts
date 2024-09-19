5727e56a21230afb28e8b3beaaf980c35466694f
Mario Lemos Quirino Neto <100967409+mariolqn@users.noreply.github.com>
2024-09-19 18:14:05 -0300

    Node

--- src/markmap.ts
+++ src/markmap.ts
@@ -1,7 +1,8 @@
-import { loadJS, loadCSS } from 'markmap-common';
+import { loadCSS, loadJS } from 'markmap-common';
 import { Transformer } from 'markmap-lib';
+import * as markmap from 'markmap-view';
 
 export const transformer = new Transformer();
 const { scripts, styles } = transformer.getAssets();
 loadCSS(styles);
-loadJS(scripts);
+loadJS(scripts, { getMarkmap: () => markmap });
